0fc2387f99a7cd892c297229083afc0e6d7ca956
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:23 -0300

    chore: re index awssts

--- flake.nix
+++ flake.nix
@@ -7,38 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."awssts-main".dir   = "main";
-  inputs."awssts-main".owner = "nim-nix-pkgs";
-  inputs."awssts-main".ref   = "master";
-  inputs."awssts-main".repo  = "awssts";
-  inputs."awssts-main".type  = "github";
-  inputs."awssts-main".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."awssts-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."awssts-v1_0_0".dir   = "v1_0_0";
-  inputs."awssts-v1_0_0".owner = "nim-nix-pkgs";
-  inputs."awssts-v1_0_0".ref   = "master";
-  inputs."awssts-v1_0_0".repo  = "awssts";
-  inputs."awssts-v1_0_0".type  = "github";
-  inputs."awssts-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."awssts-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."awssts-v1_0_1".dir   = "v1_0_1";
-  inputs."awssts-v1_0_1".owner = "nim-nix-pkgs";
-  inputs."awssts-v1_0_1".ref   = "master";
-  inputs."awssts-v1_0_1".repo  = "awssts";
-  inputs."awssts-v1_0_1".type  = "github";
-  inputs."awssts-v1_0_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."awssts-v1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."awssts-v1_0_2".dir   = "v1_0_2";
-  inputs."awssts-v1_0_2".owner = "nim-nix-pkgs";
-  inputs."awssts-v1_0_2".ref   = "master";
-  inputs."awssts-v1_0_2".repo  = "awssts";
-  inputs."awssts-v1_0_2".type  = "github";
-  inputs."awssts-v1_0_2".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."awssts-v1_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
